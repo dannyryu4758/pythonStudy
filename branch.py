c0a4a9b1a27ceb6c117d8dd0e4edb21917ef4107
dannyryu4758 <loveme6230@naver.com>
2021-02-15 23:32:21 +0900

    파이썬 공부 (함수까지)

---
+++ branch.py
@@ -0,0 +1,103 @@
+# # 조건문
+#  if
+# if 조건:
+#     실행 명령문
+
+# weather = input("오늘 날씨 어때요?")
+
+# if weather == "비" or weather == "눈":
+#     print("우산을 챙기세요")
+# elif weather == "미세먼지":
+#     print("마스크를 챙기세요")
+# else:
+#     print("준비물 필요 없어요.")
+
+
+# temp = int(input("기온은 어때요?"))
+# if 30 <= temp:
+#     print("너무 더워요. 나가지 마세요")
+# elif 10 <= temp and temp < 30:
+#     print("괜찮은 날씨예요")
+# elif 0 <= temp < 10:
+#     print("외투를 챙기세요")
+
+# else:
+#     print("너무 추워요. 나가지 마세요")
+
+
+########################################################
+
+# # 반복문
+
+# for
+# for waiting_no in [0, 1, 2, 3, 4]:
+#     print("대기번호 : {0}".format(waiting_no))
+
+# for waiting_no in range(5):  # 0,1,2,3,4
+# for waiting_no in range(1, 6):  # 1,2,3,4,5
+#     print("대기번호 : {0}".format(waiting_no))
+
+# starbucks = ["아이언맨", "토르", "아엠 그루트"]
+# for customer in starbucks:
+#     print("{0}, 커피가 준비되었습니다.".format(customer))
+
+
+# while
+# customer = "토르"
+# index = 5
+# while index >= 1:
+#     print("{0}, 커피가 준비 되었습니다. {1} 번 남았어요".format(customer, index))
+#     index -= 1
+#     if index == 0:
+#         print("커피는 폐기처분되었습니다.")
+
+
+# customer = "토르"
+# person = "Unknown"
+# while person != customer:
+#     print("{0}, 커피가 준비되었습니다".format(customer))
+#     person = input("이름이 어떻게 되세요?")
+
+
+# absent = [2, 5]  # 결석
+# no_book = [7]  # 책을 가져오지 않은 학생
+# for student in range(1, 11):  # 1 ~ 10
+#     if student in absent:
+#         continue
+#     elif student in no_book:
+#         print("오늘 수업 여기까지. {0}는 교무실로 따라와".format(student))
+#         break
+#     print("{0}, 책을 읽어봐".format(student))
+
+
+# # 한줄 for 문
+
+# 출석번호가 1 2 3 4, 앞에 100을 붙이기로함 => 101,102,103,104
+# students = [1, 2, 3, 4, 5]
+# print(students)
+# students = [i+100 for i in students]
+# print(students)
+
+# 학생 이름을 길이로 변환
+# students = ["Iron man", "Thor", "I am groot"]
+# students = [len(i) for i in students]
+# print(students)
+
+# 학생 이름을 대문자로 변환
+# students = ["Iron man", "Thor", "I am groot"]
+# students = [i.upper() for i in students]
+# print(students)
+
+# 퀴즈
+from random import *
+client_no = 1
+total_client = 0
+while client_no < 51:
+    time = randint(5, 51)
+    if 5 <= time <= 15:
+        print("[O] {0}번째 손님 (소요시간 : {1}분)".format(client_no, time))
+        total_client += 1
+    else:
+        print("[ ] {0}번째 손님 (소요시간 : {1}분)".format(client_no, time))
+    client_no += 1
+print("총 탑승 승객 : ", total_client, "분")
--- practice.py
+++ practice.py
@@ -1,22 +1,33 @@
+# 양수로 만들기
 # print(abs(-5)) # 5
+# O 제곱 구하기
 # print((pow(4,2))) # 4^2 = 4*4 = 16
+# 큰값/작은값 구하기
 # print(max(5,12)) # 12
 # print(min(5,12)) # 5
+# 반올림하기
 # print(round(3.24)) # 3
 # print(round(4.99)) # 5
 
+###########################################################
+
+# # math 라이브러리
 # from math import *
 # print(floor(4.99)) # 내림 4
 # print(ceil(3.14)) # 올림 4
-# print(sqrt(16)) # 제곱근 4
+# print(sqrt(16)) # 제곱근 4 (루트)
 
+# 3
+
+# # random 라이브러리
 # from random import *
-# print(random()) # 0.0 ~ 1.0 미만의 임의의 값 생성
-# print(randrange(1,46)) # 1 ~ 46 미만의 임의의 값 생성
+# print(random()) # 0.0 ~ 1.0 미만의 임의의 값 생성 (소수점)
+# print(randrange(1,46)) # 1 ~ 46 미만의 임의의 값 생성 (정수)
 # print(randint(1,45)) # 1 ~ 45 이하의 임의의 값 생성
 
-# # 리스트 []
+###########################################################
 
+# # 리스트 []
 # # 지하철 칸별로 10명, 20명, 30명
 # subway1 = 10
 # subway2 = 20
@@ -31,7 +42,7 @@
 # # 조세호씨가 몇번째 칸에 타고 있는지
 # print(subway.index("조세호"))
 
-# # 하하씨가 다음 정류장에서 다음칸에 탐
+# # 하하가 다음 정류장에서 다음칸에 탐
 # subway.append("하하")
 # print(subway)
 
@@ -67,16 +78,143 @@
 # print(mix_list)
 
 # # 리스트 확장
-# num_list.extend(mix_list)
+# num_list.extend(mix_list) # 두 리스트 하나로 합치기
 # print(num_list)
 
-# 사전
-cabinet = {3: "유재석", 100: "김태호"}
-print(cabinet[3])
-print(cabinet[100])
+###########################################################
 
-print(cabinet.get(3))
+# # 사전
+# cabinet = {3: "유재석", 100: "김태호"}
+# print(cabinet[3])
+# print(cabinet[100])
+
+# print(cabinet.get(3))
 # print(cabinet[5]) # index 에러 (프로그램 종료)
-print(cabinet.get(5, "사용가능"))  # None 출력, + 기본값 (프로그램 진행)
-print(cabinet.get(5, "사용가능"))  # 기본값 설정 (프로그램 진행)
-print("hi")
+# print(cabinet.get(5, "사용가능"))  # None 출력, + 기본값 (프로그램 진행)
+# print(cabinet.get(5, "사용가능"))  # 기본값 설정 (프로그램 진행)
+# print("hi")
+
+# # 사전 자료형 안에 원하는 값이 있는지 확인
+# print(3 in cabinet)
+# print(5 in cabinet)
+
+# cabinet = {"A-3": "유재석", "B-100": "김태호"}
+# print(cabinet["A-3"])
+# print(cabinet["B-100"])
+
+# # 새 손님
+# print(cabinet)
+# # 새로운 값 추가
+# cabinet["C-20"] = "조세호"
+# # 기존 값 변경
+# cabinet["A-3"] = "김종국"
+# print(cabinet)
+
+# # 나간 손님
+# del cabinet["A-3"]
+# print(cabinet)
+
+# # key 들만 출력
+# print(cabinet.keys())
+
+# # value 들만 출력
+# print(cabinet.values())
+
+# # key, value 쌍으로 출력
+# print(cabinet.items())
+
+# # 목욕탕 폐점
+# cabinet.clear()
+# print(cabinet)
+
+
+#############################################
+
+# # 튜플 : 내용의 변경 및 추가가 되지 않음 (속도 list보다 빠름)
+
+# menu = ("돈까스", "치즈까스")
+# print(menu[0])
+# print(menu[1])
+
+# name = "김종국"
+# age = 20
+# hobby = "코딩"
+# print(name, age, hobby)
+
+# # 튜플을 활용해 한번에 값을 나눠서 넣기
+# (name, age, hobby) = ("김종국", 20, "코딩")
+# print(name, age, hobby)
+
+################################################
+
+# # 집합 (set)
+# 중복 안됨, 순서 없음
+# my_set = {1, 2, 3, 3, 3}
+# print(my_set)
+
+# java = {"유재석", "김태호", "양세형"}
+# python = set(["유재석", "박명수"])
+
+# 교집합 (java 와 python을 모두 할 수 있는 사람)
+# print(java & python)  # 유재석
+# print(java.intersection(python))  # 유재석
+
+# 합집합 (java도 할 수 있거나 python을 할 수 있는 개발자)
+# print(java | python)
+# print(java.union(python))
+
+# 차집합 (java는 할 수 있지만 python을 할 수 없는 개발자)
+# print(java - python)
+# print(java.difference(python))
+
+# python 할 줄 아는 사람이 늘어남
+# python.add("김태호")
+# print(python)
+
+# java를 잊어버렸어요
+# java.remove("김태호")
+# print(java)
+
+#########################################################
+
+# # 자료구조의 변경
+# 커피숍
+
+# menu = {"커피", "우유", "주스"}
+# print(menu, type(menu))
+
+# menu = list(menu)
+# print(menu, type(menu))
+
+# menu = tuple(menu)
+# print(menu, type(menu))
+
+# menu = set(menu)
+# print(menu, type(menu))
+
+####################################################
+
+# # 추첨 기능
+from random import *
+# lst = [1, 2, 3, 4, 5]
+# print(lst)
+# # 순서 섞기
+# shuffle(lst)
+# print(lst)
+# # 샘플로 무작위로 하나 뽑기
+# print(sample(lst, 1))
+
+users = range(1, 21)  # 1 ~ 20까지숫자르 ㄹ생성
+# print(type(users))  # 타입이 range
+users = list(users)  # 자료 타입 변환
+# print(type(users))  # 타입이 list
+
+print(users)
+shuffle(users)
+print(users)
+
+winners = sample(users, 4)  # 4 명 중에서 1명은 치킨, 3명은 커피
+print("-- 당첨자 발표 --")
+print("치킨 당첨자 : {0}".format(winners[0]))
+print("커피 당첨자 : {0}".format(winners[1:]))
+print("-- 축하합니다 --")
